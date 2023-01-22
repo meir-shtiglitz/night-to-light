baed599d1839787d2b0f9ecebb8971cdca524d44
Meir <m.stigel@gmail.com>
2023-01-22 20:32:25 +0200

    changed route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './components/style.css'
 // import Nav from './components/nav';
 import Lights from './components/lights';
 import { Switch, Route } from "react-router-dom";
@@ -9,7 +10,7 @@ function App() {
   return (
     <>
       <Switch>
-        <Route path="/lights" component={Lights} />
+        <Route path="/" component={Lights} />
       </Switch>
     </>
   );
